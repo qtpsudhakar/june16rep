6e8e5aaa467312e6fa78ba636e608ba9625999e1
qtpsudhakar <sudhakar@qtpsudhakar.com>
2017-10-06 21:59:28 +0530

    Update Demo1.java

--- src/test/java/com/tng/ohrm/Demo1.java
+++ src/test/java/com/tng/ohrm/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("develop");
 		System.out.println("second change");
+		System.out.println(" change from remote");
 	}
 
 }
